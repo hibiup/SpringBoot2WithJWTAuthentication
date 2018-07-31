a8134de76f5b0f44f19b0fab1f8d6ac4e9904abb
Jeff W <jeffwji@gmail.com>
2018-07-30 23:41:52 -0400

    Add jetty maven plugin as comment

--- src/test/java/samples/springboot2/it/HelloSpringBootIT.java
+++ src/test/java/samples/springboot2/it/HelloSpringBootIT.java
@@ -1,6 +1,5 @@
 package samples.springboot2.it;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/samples/springboot2/unit/HelloSpringBootTest.java
+++ src/test/java/samples/springboot2/unit/HelloSpringBootTest.java
@@ -1,6 +1,5 @@
 package samples.springboot2.unit;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
